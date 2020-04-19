bac2b4fba1b528da53034908cb34e0b7811b5c77
Yasen <362yasen2@gmail.com>
2020-04-18 23:18:37 -0700

    first commit

---
+++ index/js/main.js
@@ -0,0 +1,41 @@
+//select Dom element
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNev = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItem = document.querySelectorAll(".nav-item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menuBranding.classList.add("show");
+    menuNev.classList.add("show");
+    menu.classList.add("show");
+    navItem.forEach(item => {
+      item.classList.add("show");
+    });
+    showMenu = true;
+  } else {
+    showMenu = false;
+    menuBtn.classList.remove("close");
+    menuBranding.classList.remove("show");
+    menuNev.classList.remove("show");
+    menu.classList.remove("show");
+    navItem.forEach(item => {
+      item.classList.remove("show");
+    });
+  }
+}
+
+function copyToClipboard(text) {
+  const el = document.createElement("textarea");
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+}
